9d398f66e60d813edb3f5bc526a2a224f4ec2e6d
Zeline <juvictoriad@gmail.com>
2026-03-05 21:24:36 +0000

    feat: implement database-cached weather

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from .availability import Availability
 from .station import Station
 from .user import User
+from .weather import WeatherForecast
 
-__all__ = ["Station", "Availability", "User"]
+__all__ = ["Station", "Availability", "User", "WeatherForecast"]
